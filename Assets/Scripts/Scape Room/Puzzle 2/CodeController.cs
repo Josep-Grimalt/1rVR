51fdc9a3766d46a496016d4385ef43a9f88e21ba
Josep-Grimalt <josepmasg@gmail.com>
2025-02-18 15:36:16 +0100

    Update CodeController.cs

--- Assets/Scripts/Scape Room/Puzzle 2/CodeController.cs
+++ Assets/Scripts/Scape Room/Puzzle 2/CodeController.cs
@@ -24,6 +24,7 @@ public class CodeController : MonoBehaviour
         if (codeText.text.Equals("8624"))
         {
             eventManager.CodeEntered();
+            GameObject.Find("Ray Interactor").gameObject.SetActive(false);
         }
         else
         {
