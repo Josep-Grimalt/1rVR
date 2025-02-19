6b49195f3d8af54878b75aee1d09bc931eeaeeff
Josep-Grimalt <josepmasg@gmail.com>
2025-02-19 20:31:12 +0100

    Update MagGenerator.cs

--- Assets/Scripts/Pistol/Mag/MagGenerator.cs
+++ Assets/Scripts/Pistol/Mag/MagGenerator.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class MagGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject mag;
+    [SerializeField] private XRSocketInteractor socket;
 
     public void GenerateMag()
     {
         GameObject go = Instantiate(mag, transform.position, transform.rotation);
 
         go.transform.SetParent(transform);
+        socket.interactablesSelected.Add(go.GetComponent<XRGrabInteractable>());
     }
 }
